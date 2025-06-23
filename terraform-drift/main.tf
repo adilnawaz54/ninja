1794c9425e0714023bb7290d8f783aecf66486b4
adilnawaz54 <adilnawaz54@gmail.com>
2025-06-23 16:42:42 +0530

    Create main.tf

---
+++ terraform-drift/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "demo" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  root_block_device {
+    volume_size           = var.volume_size   # <-- 20 GB root volume
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+ tags = {
+    Name = "Terraform-Drift-Demo"
+  }
+}

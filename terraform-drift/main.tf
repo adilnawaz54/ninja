3be1a91c92e51c4e023e4af889ea76c96113e793
adilnawaz54 <adilnawaz54@gmail.com>
2025-06-23 18:02:10 +0530

    Update main.tf

--- terraform-drift/main.tf
+++ terraform-drift/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-bucket54"
+    bucket         = "tfstate-bucket-54"
     key            = "terraform-drift-demo/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
